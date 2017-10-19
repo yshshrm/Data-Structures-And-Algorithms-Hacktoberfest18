8ee3e67891168648939cd65d01604c762c93f686
Raju Vishwas <rajuvishwas@gmail.com>
2017-10-19 14:58:47 +0700

    #1 - Binary Search in PHP

--- php/binary_search.php
+++ php/binary_search.php
@@ -3,21 +3,21 @@
 class BinarySearch
 {
 
-	static public function search($value, $array)
-	{
-		$top = sizeof($array) - 1;
-		$bottom = 0;
+    static public function search($value, $array)
+    {
+        $top = sizeof($array) - 1;
+        $bottom = 0;
 
-		while($top >= $bottom) {
+        while ($top >= $bottom) {
 
-			$middle = floor( ($top + $bottom) / 2);
-			if($array[$middle] < $value) $bottom = $middle + 1;
-			elseif ( $array[$middle] > $value) $top = $middle - 1;
-			else return true;
+            $middle = floor(($top + $bottom) / 2);
+            if ($array[$middle] < $value) $bottom = $middle + 1;
+            elseif ($array[$middle] > $value) $top = $middle - 1;
+            else return true;
 
-		}
+        }
 
-		return false;
-	}
+        return false;
+    }
 
 }
