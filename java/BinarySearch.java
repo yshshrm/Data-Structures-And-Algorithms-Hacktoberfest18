b00e37d3de1ef5aa5466a3832d572aac3a599dc8
Rohit Awate <rohitawate121@gmail.com>
2017-10-19 13:33:45 +0530

    Added simple Binary Search using Java

---
+++ java/BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+    public boolean search(int arr[], int key){
+        int low = 0;
+        int high = arr.length - 1;
+        int mid, swap;
+
+        for(int i=arr.length-1; i>0; i--)
+            for(int j=0; j<i; j++){
+                if(arr[j] > arr[j+1]){
+                    swap = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = swap;
+                }
+            }
+
+        while(low <= high){
+            mid = (low + high) / 2;
+
+            if(arr[mid] == key)
+                return true;
+            else if(key > arr[mid])
+                low = mid + 1;
+            else if(key < arr[mid])
+                high = mid - 1;
+        }
+        return false;
+    }
+}
