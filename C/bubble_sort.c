24378c65e94da133ea4b49599a963201fdefb430
João Faustino <joaoplfaustino@gmail.com>
2017-10-20 23:33:14 +0000

    minor typo.

--- C/bubble_sort.c
+++ C/bubble_sort.c
@@ -23,8 +23,8 @@ int main(){
     // bubble sort
     int j, swap;
 
-    for (i = 0; i < (size - 1); i++){
-        for (j = 0; j < size - i - 1; j++){
+    for (i = 0; i < size; i++){
+        for (j = 0; j < size - i; j++){
             if (array[j] > array[j + 1]){
                 swap = array[j];
                 array[j] = array[j + 1];
