49c440220fc9bbcb86752322e22baef1954f48cc
sseficha <32609628+sseficha@users.noreply.github.com>
2017-10-22 15:51:02 +0300

    Update binary_search.java

--- java/Search/binary_search.java
+++ java/Search/binary_search.java
@@ -6,7 +6,7 @@ public class BinarySearch {
 
         for(int i=arr.length-1; i>0; i--)
             for(int j=0; j<i; j++){
-                if(arr[j] > arr[j+1]){
+                if(arr[j] > arr[j+1]){===5y4w  rhgwsdserd
                     swap = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = swap;
