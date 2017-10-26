8c167823b1d6e4e5819acd7ca6b607342162f44c
djsdvg <leonardo.ballini@protonmail.com>
2017-10-26 08:48:03 +0200

    Add comments

--- java/Sorts/SelectionSort.java
+++ java/Sorts/SelectionSort.java
@@ -8,13 +8,13 @@ public class SelectionSort<E> {
         if(arr.length == 0) return;
 
         for(i = 0; i < arr.length-1; i++){
-            min = i;
+            min = i; //suppose that the first number of our subarray is the smallest
             for(j = i+1; j < arr.length; j++){
-                if(arr[j].compareTo(arr[min]) < 0) min = j;
+                if(arr[j].compareTo(arr[min]) < 0) min = j; //if I find a smaller number I update the variable min
             }
-            if(arr[min].compareTo(arr[i]) != 0){
-                tmp = arr[min];
-                arr[min] = arr[i];
+            if(arr[min].compareTo(arr[i]) != 0){//if a swap is needed...
+                tmp = arr[min];                 //i swap the position of the first number of the subarray...
+                arr[min] = arr[i];             //with the smallest number of the subarray
                 arr[i] = tmp;
             }
         }
