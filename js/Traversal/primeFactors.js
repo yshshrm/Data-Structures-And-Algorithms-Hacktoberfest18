55459a973a1f3be1f809cfac27a892c9cac87070
Maciej Bregisz <maciox55@gmail.com>
2018-10-01 13:58:59 -0400

    Prime Number algorithms

---
+++ js/Traversal/primeFactors.js
@@ -0,0 +1,15 @@
+function primeFactors(n){
+  var factors = [], 
+      divisor = 2;
+  
+  while(n>2){
+    if(n % divisor == 0){
+       factors.push(divisor); 
+       n= n/ divisor;
+    }
+    else{
+      divisor++;
+    }     
+  }
+  return factors;
+}
