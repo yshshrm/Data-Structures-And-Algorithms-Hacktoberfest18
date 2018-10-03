61817005db2d6ef2bf38bf38a9ccebe624e3e3c0
Rene <renespa@web.de>
2018-10-03 12:34:52 +0200

    Added Palindrome check

---
+++ csharp/strings/Palindrome.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Palindrome
+{
+    static bool isPalindrome(string input)
+    {
+        if (input.Length == 0) return false;
+        if (input.Length == 1) return true;
+
+        for(int i = 0; i < input.Length/2; i++)
+        {
+            if(char.ToLower(input[i]) != char.ToLower(input[input.Length-1-i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Test
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Test Palindrome:");
+        Console.WriteLine("Fun :" + isPalindrome("Fun"));
+        Console.WriteLine("Anna :" + isPalindrome("Anna"));
+        Console.WriteLine("Anhna :" + isPalindrome("Anhna"));
+        Console.WriteLine("Test :" + isPalindrome("Test"));
+        Console.ReadKey();
+    }
+}
