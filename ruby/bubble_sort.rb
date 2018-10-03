ef5aac1210cbbdeea877d03fdbef782e85e9bbc5
Coderjolly <harmanjolly97@gmail.com>
2018-10-04 00:32:26 +0530

    Added bubble sort

---
+++ ruby/bubble_sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  array
+end
