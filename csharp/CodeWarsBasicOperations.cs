2e8fde81da96b3a5dcad8fa6eac44ff7c605fb06
Evie-Skinner18 <eskinner@spartaglobal.com>
2018-10-04 20:11:03 +0100

    Added a basic algorithm in Csharp

---
+++ csharp/CodeWarsBasicOperations.cs
@@ -0,0 +1,28 @@
+﻿namespace Solution
+{
+    public static class Program
+    {
+        public static double basicOp(char operation, double value1, double value2)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case ('+'):
+                    result = value1 + value2;
+                    break;
+                case ('-'):
+                    result = value1 - value2;
+                    break;
+                case ('*'):
+                    result = value1 * value2;
+                    break;
+                case ('/'):
+                    result = value1 / value2;
+                    break;
+            }
+            // end of switch
+            return result;
+        }
+    }
+}
