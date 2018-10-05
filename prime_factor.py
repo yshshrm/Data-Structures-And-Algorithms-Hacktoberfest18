eca9ddc1db7a268398faa6093b84a642e47b02ae
Amarjeet Singh <33446077+amarjeet045@users.noreply.github.com>
2018-10-05 23:53:26 +0530

    Create prime_factor.py

---
+++ prime_factor.py
@@ -0,0 +1,16 @@
+#this is a program to find the largest prime factor of a number
+#here t is the test case and n is the number of input
+t = int(input())
+while(t):
+    t-=1
+    n = int(input())
+    i=2
+    while (i*i<=n):
+        if(n%i==0):
+            n = int(n/i)
+        i+=1
+    if(n>2):
+        i=n
+        print(i)
+
+    
