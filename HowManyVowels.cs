5b9286c3b9d5bbe574f1356ce4f46ea9593b5ec6
Evie-Skinner18 <eskinner@spartaglobal.com>
2018-10-06 11:29:37 +0100

    Added an algorithm that counts the number of vowels in a given string

---
+++ HowManyVowels.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using System.Linq;
+
+
+namespace CodewarsAlgorithms
+{
+
+
+    public static class Kata
+
+    {
+
+        public static int GetVowelCount(string str)
+
+        {
+
+            int vowelCount = 0;
+
+
+
+            // Your code here
+
+            char[] letters = str.ToCharArray();
+
+
+
+            foreach (char c in letters)
+
+            {
+
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+
+                {
+
+                    vowelCount++;
+
+                }
+
+            }
+
+
+
+            return vowelCount;
+
+        }
+
+    }
+}
