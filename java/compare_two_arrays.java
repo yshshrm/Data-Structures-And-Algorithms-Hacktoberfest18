a5132114f411e7968e4d0b37a65660a30a8a3db6
sethlution <sethlution@gmail.com>
2018-10-06 20:15:59 +0700

    Fixed the code
    - Changed return type to List object
    - Initialize the two iterators in the for loops

--- java/compare_two_arrays.java
+++ java/compare_two_arrays.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class compare_two_arrays {
 	
-	public Array findCommonNumbers(int[] a, int[] b){
+	public List findCommonNumbers(int[] a, int[] b){		
 		List<Integer> commonNumber = new ArrayList();
-		for(int i; i < a.length;i++){
-			for(int j;j < b.length;j++){
+		for(int i=0; i < a.length;i++){
+			for(int j=0; j < b.length;j++){
 				if(a[i] == b[j]){
 					commonNumber.add(a[i]);
 				}
