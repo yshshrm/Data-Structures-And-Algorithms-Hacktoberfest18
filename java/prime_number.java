3e8eb67e41c27ad0895777e1a0d9b542de44df2a
Vineesh Gupta <vineeshgupta898@gmail.com>
2018-10-06 23:03:53 +0530

    Update prime_number.java

--- java/prime_number.java
+++ java/prime_number.java
@@ -1,26 +1,21 @@
 public class Prime
 {    
     public static void main(String args[])
-    {    
-        int i,m=0,flag=0;      
-        int n=3;//it is the number to be checked    
-        m=n/2;      
-        if(n==0||n==1)
-        {  
-          System.out.println(n+" is not prime number");      
+    {          
+        int value=3;//it is the number to be checked    
+       if((value > 2 && value % 2 == 0) || value == 1) {
+           System.out.println("Not a Prime number");
+                return ;
         }
-        else
-        {  
-          for(i=2;i<=m;i++)
-          {      
-            if(n%i==0)
-            {      
-              System.out.println(n+" is not prime number");      
-              flag=1;      
-              break;      
-            }      
-          }      
-        if(flag==0)  { System.out.println(n+" is prime number"); }  
-        }//end of else  
+
+        for (int i = 3; i <= (int)Math.sqrt(value); i += 2) {
+
+            if (value % i == 0) {
+              System.out.println("Not a Prime number");
+                return ;
+            }
+        }
+
+       System.out.println(" Prime number");
     }    
 }   
