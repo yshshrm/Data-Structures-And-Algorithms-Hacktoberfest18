8ffe464cea2d03dc67f2a8c44833f25c0f9fccda
TheCharmingSociopath <aditya.morolia@gmail.com>
2018-10-06 15:47:40 +0530

    added Square root using newtons method, Merge Sort, Binary search and transpose of matrix in C

---
+++ c/algorithm/BinarySearch.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+	int a[10],i,j,ch,large,f=0,small,mid;
+	for(i=0;i<10;i++)
+		scanf("%d",&a[i]);
+	scanf("%d",&ch);
+	large=9;small=0;mid=4;
+	while(mid!=ch&&small!=large)
+	{
+		if(ch<mid)
+		large=mid-1;
+		else
+		small=mid+1;
+		mid=(large+small)/2;
+		if(ch==mid)
+		{
+			f=1;
+			break;
+		}
+	}
+	if(f)
+		printf("Choice Found");
+	else
+		printf("Choice not found");
+}
