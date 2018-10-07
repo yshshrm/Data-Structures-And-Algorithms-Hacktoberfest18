b0deea406735e2244fc1e3bd591ef0f9fad17adc
Mahmoud Mohamed Anwar <mamoanwar97@gmail.com>
2018-10-07 16:18:04 +0200

    modified string to int file

--- cpp/StringToInt_ViceVersa.cpp
+++ cpp/StringToInt_ViceVersa.cpp
@@ -1,33 +1,28 @@
-//convert number to string
-/*
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main()
-{   int x;
-    string n;
-    cin>>n;
-    x=atoi(n.c_str());
-    cout << x <<endl << x+1;
-   return 0;
-}*/
-    //----------------------------------------------------------------------------------------------------------
-//convert number to string
 #include <iostream>
 #include <String>
 #include <iomanip>
 #include <locale>
 #include <sstream>
+
 using namespace std;
-int main()
+
+int StringToInt(string n)
 {   int x;
+    x=atoi(n.c_str());
+   return x;
+}
+string IntToString(int x){
     string n;
     ostringstream kz;
-    cin >> x;
     kz << x;
     n=kz.str();
-    cout << n[2];
-    kz.str("");
+    return n;
+}
+
+int main()
+{
+    int x=6;
+    string n="64";
+    cout << x + StringToInt(n) << endl << IntToString(x)+ n;
    return 0;
 }
