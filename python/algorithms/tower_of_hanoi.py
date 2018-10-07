c97d5d6b287f825496c80b7e7cc9f8b991ade12b
dumbcoder2399 <anusvar2399@gmail.com>
2018-10-07 13:44:31 +0530

    adds to Tower of hanoi python

--- python/algorithms/tower_of_hanoi.py
+++ python/algorithms/tower_of_hanoi.py
@@ -1,5 +1,5 @@
 def hanoi(n, source, target, auxiliary):
-    if n > 0:
+    if n >=1:
         hanoi(n-1, source, auxiliary, target)
         print('Move disk %d from %s -> %s' % (n, source, target))
         hanoi(n-1, auxiliary, target, source)
