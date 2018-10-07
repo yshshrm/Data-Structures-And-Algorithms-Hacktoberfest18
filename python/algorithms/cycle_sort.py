2a56f68d36e3084af9e399600e0e3607845c1910
Jaroslav Urban <42768686+JaroslavUrbann@users.noreply.github.com>
2018-10-07 10:59:07 +0200

    Create cycle_sort.py

---
+++ python/algorithms/cycle_sort.py
@@ -0,0 +1,40 @@
+def cycleSort(array): 
+  writes = 0
+    
+  # Loop through the array to find cycles to rotate. 
+  for cycleStart in range(0, len(array) - 1): 
+    item = array[cycleStart] 
+      
+    # Find where to put the item. 
+    pos = cycleStart 
+    for i in range(cycleStart + 1, len(array)): 
+      if array[i] < item: 
+        pos += 1
+      
+    # If the item is already there, this is not a cycle. 
+    if pos == cycleStart: 
+      continue
+      
+    # Otherwise, put the item there or right after any duplicates. 
+    while item == array[pos]: 
+      pos += 1
+    array[pos], item = item, array[pos] 
+    writes += 1
+      
+    # Rotate the rest of the cycle. 
+    while pos != cycleStart: 
+        
+      # Find where to put the item. 
+      pos = cycleStart 
+      for i in range(cycleStart + 1, len(array)): 
+        if array[i] < item: 
+          pos += 1
+        
+      # Put the item there or right after any duplicates. 
+      while item == array[pos]: 
+        pos += 1
+      array[pos], item = item, array[pos] 
+      writes += 1
+    
+  return writes
+  
