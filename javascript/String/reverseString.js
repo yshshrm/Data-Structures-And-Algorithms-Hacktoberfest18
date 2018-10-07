0021c204e7c4202dc2a61caa18884ebe703f91aa
David Bambusek <bambusekd@gmail.com>
2018-10-07 12:39:02 +0200

    another JS string reverse algorithms

--- javascript/String/reverseString.js
+++ javascript/String/reverseString.js
@@ -6,5 +6,19 @@ function reverse(str) {
     return revd;
 }
 
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+function reverseStringRecursion(str) {
+    if(str === ''){
+        return '';
+    } else {
+        return reverseStringRecursion(str.substring(1)) + str.charAt(0);
+    }
+}
+
 var word = "ocean";
 console.log(reverse(word));
+console.log(reverseString(word));
+console.log(reverseStringRecursion(word));
