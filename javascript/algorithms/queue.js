294782b64b6071883d4108e1c4ee3b1a22c60842
Yuttasak Pannawat <yuttasak@hot-now.com>
2018-10-09 23:34:20 +0700

    Add Queue and Stack to javascript algorithms.

---
+++ javascript/algorithms/queue.js
@@ -0,0 +1,7 @@
+let queue = [];
+
+//put value on end of queue
+queue.push(1);
+
+//Take first value from queue
+let value = queue.shift();
---
+++ javascript/algorithms/stack.js
@@ -0,0 +1,7 @@
+let stack = [];
+
+//put value on top of stack
+stack.push(1);
+
+//remove value from top of stack
+let value = stack.pop();
