634929ddd7423973f68870b3fb5dd868e815226c
D.Sunil <sunilnitdgp5@gmail.com>
2018-10-11 00:36:51 +0530

    Added the code which will work for large no.'s
    The code will work if the input is large.

--- java/algorithms/factorial.java
+++ java/algorithms/factorial.java
@@ -1,6 +1,48 @@
-class factorial{
-    int fact(int n){
-        if(n==1) return 1;
-        else return n * fact(n-1);
-    }
-}
+class GFG {  
+    static void factorial(int n) 
+    { 
+        int res[] = new int[500]; 
+  
+        // Initialize result 
+        res[0] = 1; 
+        int res_size = 1; 
+  
+        // Apply simple factorial formula  
+        // n! = 1 * 2 * 3 * 4...*n 
+        for (int x = 2; x <= n; x++) 
+            res_size = multiply(x, res, res_size); 
+  
+        System.out.println("Factorial of given number is "); 
+        for (int i = res_size - 1; i >= 0; i--) 
+            System.out.print(res[i]); 
+    } 
+    static int multiply(int x, int res[], int res_size) 
+    { 
+        int carry = 0; // Initialize carry 
+  
+        // One by one multiply n with individual  
+        // digits of res[] 
+        for (int i = 0; i < res_size; i++) 
+        { 
+            int prod = res[i] * x + carry; 
+            res[i] = prod % 10; // Store last digit of  
+                                // 'prod' in res[] 
+            carry = prod/10; // Put rest in carry 
+        } 
+  
+        // Put carry in res and increase result size 
+        while (carry!=0) 
+        { 
+            res[res_size] = carry % 10; 
+            carry = carry / 10; 
+            res_size++; 
+        } 
+        return res_size; 
+    } 
+  
+    // Driver program 
+    public static void main(String args[]) 
+    { 
+        factorial(100); 
+    } 
+} 
