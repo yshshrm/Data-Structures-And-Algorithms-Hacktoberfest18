dc12c0d76362b57ae31a966ace6c7cfae723af97
Shashvat Mohane <Sha.shashvat@gmail.com>
2018-10-10 11:30:59 +0530

    Coin Change Problem
    Its a coin change problem with limited Coins !
    Consider that you have only 10,20,30 rs. coin !

---
+++ c/algorithm/CoinChange.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#define c 5
+
+int main()
+{
+
+    int coin[c][2];
+    int x,i,j,a;
+    for(i=0;i<c;i++)
+       for(j=0;j<2;j++)
+       {
+           scanf("%d",&coin[i][j]);
+       }
+    printf("Enter Amount: ");
+    scanf("%d",&a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    	printf("Current fin\n");
+	printf("10 Rupee notes : %d\n",fin[0] );
+	printf("20 Rupee notes : %d\n",fin[1] );
+	printf("30 Rupee notes : %d\n",fin[2] );
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
