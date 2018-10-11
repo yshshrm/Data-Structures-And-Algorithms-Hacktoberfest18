d6bf21668dca276af9a2704937e551954dc69ee7
Bar Rotstein <barrotstein@gmail.com>
2018-10-11 21:26:43 +0300

    rust selection sort

---
+++ rust/selection_sort.rs
@@ -0,0 +1,26 @@
+fn selection_sort<T: Ord>(arr: &mut Vec<T>) {
+    _selection_sort(arr, 0);
+}
+
+fn _selection_sort<T: Ord>(arr: &mut Vec<T>, offset: usize) {
+    let mut smallest_index = offset;
+    for i in (offset+1)..arr.len() {
+        if arr[i] < arr[smallest_index] {
+            smallest_index = i;
+        }
+    }
+    if smallest_index != offset {
+        arr.swap(smallest_index, offset)
+    }
+    if offset < arr.len() {
+        _selection_sort(arr, offset + 1)
+    }
+}
+
+fn main() {
+    let mut array = vec![0, 8, 4, 7, 1, 9, 10, 5, 2, 11, 3];
+    println!("{:?}", array);
+    selection_sort(&mut array);
+    println!("{:?}", array);
+
+}
