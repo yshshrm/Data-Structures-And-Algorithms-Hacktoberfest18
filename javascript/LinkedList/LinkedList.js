5ea66760c0bdb48979e1fc5b65aa02fd639aac76
Maciej Zawilski <maciejzawilski@Maciejs-MacBook-Pro.local>
2018-10-12 01:35:55 +0200

    Added addToBeginning, removeFromEnd and removeFromBeginning methods to js LinkedList; fixed addToEnd to be rassigned to LinkedList instead of Node

--- javascript/LinkedList/LinkedList.js
+++ javascript/LinkedList/LinkedList.js
@@ -7,11 +7,35 @@ function Node(val) {
   this.val = val;
 }
 
-Node.prototype.addToEnd = function(val) {
+LinkedList.prototype.addToEnd = function(val) {
   const end = new Node(val);
-  let n = this;
+  let n = this.head;
   while (n.next) {
     n = n.next;
   }
   n.next = end;
 };
+
+LinkedList.prototype.addToBeginning = function(val) {
+	const beginning = new Node(val);
+	let temp = this.head;
+	this.head = beginning;
+	beginning.next = temp;
+}
+
+LinkedList.prototype.removeFromEnd = function() {
+	let n = this.head;
+	let previous = null;
+  while (n.next) {
+  	previous = n;
+    n = n.next;
+  }
+  previous.next = null;
+  return n.val;
+}
+
+LinkedList.prototype.removeFromBeginning = function() {
+	let n = this.head;
+	this.head = this.head.next;
+	return n.val;
+}
