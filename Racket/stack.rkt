f780d2959fe29f44cbbd3dd44563b609b90a3a6e
florinrm <florinrmfm3@gmail.com>
2018-10-12 22:50:14 +0300

    Stack and queue implementatio in Racket

---
+++ Racket/stack.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(define (push elem lst) (if (null? elem)
+                            lst
+                            (append lst (list elem))))
+(define (pop lst) (if (empty? lst)
+                      '()
+                      (reverse (cdr (reverse lst)))))
+(define (top lst) (if (empty? lst)
+                      null
+                      (car (reverse lst))))
+
+;; tests
+(push 4 '(1 42 0))
+(pop '(1 2 3 4 5 6))
+(top '(1 2 3 4 5))
+(pop '())
+(top '())
+
