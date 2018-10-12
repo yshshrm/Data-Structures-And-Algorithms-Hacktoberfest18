426a597b1341b744f1c001429327c045f1525b1d
Maciej Zawilski <maciejzawilski@Maciejs-MacBook-Pro.local>
2018-10-12 03:33:26 +0200

    Changes to LinkedList belong to another PR

--- javascript/LinkedList/LinkedList.js
+++ javascript/LinkedList/LinkedList.js
@@ -7,7 +7,7 @@ function Node(val) {
   this.val = val;
 }
 
-LinkedList.prototype.addToEnd = function(val) {
+Node.prototype.addToEnd = function(val) {
   const end = new Node(val);
   let n = this.head;
   while (n.next) {
@@ -15,27 +15,3 @@ LinkedList.prototype.addToEnd = function(val) {
   }
   n.next = end;
 };
-
-LinkedList.prototype.addToBeginning = function(val) {
-	const beginning = new Node(val);
-	let temp = this.head;
-	this.head = beginning;
-	beginning.next = temp;
-}
-
-LinkedList.prototype.removeFromEnd = function() {
-	let n = this.head;
-	let previous = null;
-  while (n.next) {
-  	previous = n;
-    n = n.next;
-  }
-  previous.next = null;
-  return n.val;
-}
-
-LinkedList.prototype.removeFromBeginning = function() {
-	let n = this.head;
-	this.head = this.head.next;
-	return n.val;
-}
