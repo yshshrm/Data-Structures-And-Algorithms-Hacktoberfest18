a7409f5d789ba5a71b97559161dbdc3967b83e80
Saifuddin Syed <23101926+saifserver@users.noreply.github.com>
2018-10-13 12:43:52 +0530

    Change function to static
    Error: non-static method cannot be referenced from a static context

--- java/java_palindrome.java
+++ java/java_palindrome.java
@@ -1,7 +1,7 @@
 
 public class java_palindrome {
 	
-	public boolean isStringAPalindrome(String testString){
+	static boolean isStringAPalindrome(String testString){
 		String reverseString = new StringBuilder(testString).reverse();
 		if(testString.equals(reverseString)){
 			return true;
