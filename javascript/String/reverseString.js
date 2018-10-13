88764ecc9fedeff6ae8ab0cca1dc3baf802d4af7
AJ Genung <DragonRydrz@me.com>
2018-10-12 20:36:12 -0400

    reverse a string in one line (prettier formatted)

--- javascript/String/reverseString.js
+++ javascript/String/reverseString.js
@@ -1,10 +1,18 @@
 function reverse(str) {
-    let revd = "";
-    for (let letter of str.split('')) {
-        revd = letter + revd;
-    }
-    return revd;
+  let revd = '';
+  for (let letter of str.split('')) {
+    revd = letter + revd;
+  }
+  return revd;
 }
 
-var word = "ocean";
+var word = 'ocean';
 console.log(reverse(word));
+
+const oneLineReverse = str =>
+  str
+    .split('')
+    .reverse()
+    .join('');
+
+console.log(oneLineReverse(word));
