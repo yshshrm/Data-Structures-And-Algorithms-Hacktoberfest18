0fe7a27aaeec264ed83cceb4c1e6cd848bec0f8c
Saifuddin Syed <23101926+saifserver@users.noreply.github.com>
2018-10-13 12:46:58 +0530

    Change StringBuilder to StringBuffer
    Error: incompatible types: StringBuilder cannot be converted to String

--- java/java_palindrome.java
+++ java/java_palindrome.java
@@ -1,9 +1,9 @@
 
 public class java_palindrome {
 	
-	static boolean isStringAPalindrome(String testString){
-		String reverseString = new StringBuilder(testString).reverse();
-		if(testString.equals(reverseString)){
+	static boolean isStringAPalindrome(StringBuffer user_input){
+		StringBuffer reverseString = new StringBuffer(user_input).reverse();
+		if(user_input.equals(reverseString)){
 			return true;
 		}
 		return false;
