f4117382ba71a26c1291f9bd025ba13f6e591db7
Yash Devikar <orion.yash@gmail.com>
2018-10-13 17:04:56 +0530

    Created a linked list for python

---
+++ python/linkedList.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+
+class SLinkedList:
+    def __init__(self):
+        self.headval = None
+
+
+    def listprint(self):
+        printval = self.headval
+        while printval is not None:
+            print (printval.dataval)
+            printval = printval.nextval
+    def AtBegining(self,newdata):
+        NewNode = Node(newdata)
+
+
+        NewNode.nextval = self.headval
+        self.headval = NewNode
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+list.headval.nextval = e2
+e2.nextval = e3
+
+list.AtBegining("Sun")
+
+list.listprint()
