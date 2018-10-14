0ce1950db77f377bfe39cdf10a104bb4578c5e80
Arijit Das <arijitad.in@gmail.com>
2018-10-14 19:43:06 +0530

    Calculate Nth fibonacci no. in log n complexity.

---
+++ cpp/algorithms/nth_fibonacci_logn.cpp
@@ -0,0 +1,24 @@
+#include <map>
+#include <iostream>
+using namespace std;
+
+#define long long long
+const long M = 1000000007; // modulo
+map<long, long> F;
+
+long f(long n) {
+	if (F.count(n)) return F[n];
+	long k=n/2;
+	if (n%2==0) { // n=2*k
+		return F[n] = (f(k)*f(k) + f(k-1)*f(k-1)) % M;
+	} else { // n=2*k+1
+		return F[n] = (f(k)*f(k+1) + f(k-1)*f(k)) % M;
+	}
+}
+
+int main(){
+	long n;
+	F[0]=F[1]=1;
+	while (cin >> n)
+	cout << (n==0 ? 0 : f(n-1)) << endl;
+}
