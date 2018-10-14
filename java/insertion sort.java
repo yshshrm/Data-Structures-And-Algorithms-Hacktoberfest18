02f68eecf1747a37205e8fcc254d1c227fa13b30
Misco <kmtsandeepanie@gmail.com>
2018-10-14 12:42:03 +0530

    insertion sort

---
+++ java/insertion sort.java
@@ -0,0 +1,28 @@
+package com.java2novice.algos;
+ 
+public class MyInsertionSort {
+ 
+    public static void main(String a[]){
+        int[] arr1 = {10,34,2,56,7,67,88,42};
+        int[] arr2 = doInsertionSort(arr1);
+        for(int i:arr2){
+            System.out.print(i);
+            System.out.print(", ");
+        }
+    }
+     
+    public static int[] doInsertionSort(int[] input){
+         
+        int temp;
+        for (int i = 1; i < input.length; i++) {
+            for(int j = i ; j > 0 ; j--){
+                if(input[j] < input[j-1]){
+                    temp = input[j];
+                    input[j] = input[j-1];
+                    input[j-1] = temp;
+                }
+            }
+        }
+        return input;
+    }
+}
