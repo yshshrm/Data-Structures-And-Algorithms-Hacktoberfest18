dad855562607f09c4aa91a8001cf352090005e08
Mohammad Azharuddin <31290090+azharcaptain20@users.noreply.github.com>
2018-10-14 20:54:15 +0530

    Update BinarySearch.c

--- c/algorithm/BinarySearch.c
+++ c/algorithm/BinarySearch.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 void main()
 {
-	int a[10],i,j,ch,large,f=0,small,mid;
-	for(i=0;i<10;i++)
-		scanf("%d",&a[i]);
-	scanf("%d",&ch);
-	large=9;small=0;mid=4;
-	while(mid!=ch&&small!=large)
+	int a[10], i , j , ch , large ,f = 0 , small , mid;
+	for(i = 0 ; i < 10 ; i++)
+		scanf("%d" , &a[i]);
+	scanf("%d" , &ch);
+	large = 9;small = 0;mid = 4;
+	while(mid != ch && small != large)
 	{
-		if(ch<mid)
-		large=mid-1;
+		if(ch < mid)
+		large = mid-1;
 		else
-		small=mid+1;
-		mid=(large+small)/2;
-		if(ch==mid)
+		small = mid+1;
+		mid = (large+small)/2;
+		if(ch == mid)
 		{
-			f=1;
+			f = 1;
 			break;
 		}
 	}
