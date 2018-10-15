86dadfe9c0e90651cecf4c90219d13ca2d581b3a
Aldo-A <44143466+Aldo-A@users.noreply.github.com>
2018-10-15 19:54:49 -0400

    Greatest Number
    Finds the greatest number of an array

---
+++ java/GreatestNum.java
@@ -0,0 +1,15 @@
+public class GreatestNum
+{
+  public static void main(String[] args)
+  {
+    int[] numbers={1,2,3,4,5,6,7,342,4324,1,13,3423,5454,534,14765,87,746,7,13298}; //Random set of selected integers
+    int greatest=0;
+    
+    for(int x=0;x<=numbers.length-1;x++) //start at the first value of the set, and move up one value at a time
+    {
+      if(numbers[x]>greatest)
+        greatest=numbers[x];
+    }
+    System.out.println("The greatest number in the set is"+greatest);
+  }
+}
