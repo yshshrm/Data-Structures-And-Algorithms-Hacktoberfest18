5cdc9e1934c9408ac32bb40d31652dadbad1c0c6
Tacolover47 <crazytacolover47@gmail.com>
2018-10-22 16:46:41 -0400

    Update join_methods.hpp

--- cpp/join_methods.hpp
+++ cpp/join_methods.hpp
@@ -3,18 +3,18 @@
 #include <string_view>
 
 template<typename SeqIt>
-inline std::string join( SeqIt first, SeqIt last, const std::string &sep )
+inline std::string join(SeqIt first, SeqIt last, const std::string &sep)
 {
-    auto binaryJoinString = [sep]( std::string &a, std::string_view b ) -> std::string & {
-        return a.append((a.empty()) ? "" : sep ).append( b );
-    };
-    return std::accumulate( first, last,
-                            std::string(), binaryJoinString );
+	auto binaryJoinString = [sep](std::string &a, std::string_view b) -> std::string & {
+		return a.append((a.empty()) ? "" : sep).append(b);
+	};
+	return std::accumulate(first, last,
+		std::string(), binaryJoinString);
 }
 
 template<typename Container = std::vector<std::string >>
-inline std::string join( const Container &container,
-                         const std::string &sep )
+inline std::string join(const Container &container,
+	const std::string &sep)
 {
-    return join( container.cbegin(), container.cend(), sep );
+	return join(container.cbegin(), container.cend(), sep);
 }
