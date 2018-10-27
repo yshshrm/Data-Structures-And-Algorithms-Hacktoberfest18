a3033f8cb91b2c1d5b9502875cbef60d08d9b173
MAYANK-BHARDWAJ <44526282+MAYANK-BHARDWAJ@users.noreply.github.com>
2018-10-27 20:57:03 +0530

    Update Primality Testing.cpp

--- cpp/algorithms/Primality Testing.cpp
+++ cpp/algorithms/Primality Testing.cpp
@@ -8,8 +8,7 @@ using namespace std;
 bool isPrime(int n)
 {
 	if(n < 2)	return false;
-	if(n == 2)	return true;
-	for(int i=3; i<=sqrt(n); i+=2)
+	for(int i=2; i<=sqrt(n); i++)
 	{
 		if(n % i == 0)
 			return false;
