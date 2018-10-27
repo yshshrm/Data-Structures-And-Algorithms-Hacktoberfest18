1eb058ac7ff676df26009a9f16ccfd21687a40b7
MAYANK-BHARDWAJ <44526282+MAYANK-BHARDWAJ@users.noreply.github.com>
2018-10-27 20:49:24 +0530

    Update Kadane_Largest Sum in Contiguous Subarray.cpp

--- cpp/algorithms/Kadane_Largest Sum in Contiguous Subarray.cpp
+++ cpp/algorithms/Kadane_Largest Sum in Contiguous Subarray.cpp
@@ -7,18 +7,15 @@ using namespace std;
 
 int maxSubArraySum(int a[], int size)
 {
-    int max_so_far = INT_MIN, max_ending_here = 0;
+    int c=a[0],m=a[0];
+	    
+	    for(int i=1;i<size;i++){
 
-    for (int i = 0; i < size; i++)
-    {
-        max_ending_here = max_ending_here + a[i];
-        if (max_so_far < max_ending_here)
-            max_so_far = max_ending_here;
-
-        if (max_ending_here < 0)
-            max_ending_here = 0;
-    }
-    return max_so_far;
+            c=max(a[i],c+a[i]);
+            m=max(m,c);
+	        
+	    }
+    return m;
 }
 
 /*Driver program to test maxSubArraySum*/
