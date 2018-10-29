7402772e1897a6e9f646b24af56af5dfac7a37d3
wannafight234 <44204514+wannafight234@users.noreply.github.com>
2018-10-29 17:00:36 +0530

    Create Calendar.py

---
+++ Calendar.py
@@ -0,0 +1,11 @@
+# Python program to explain working of setfirstweekday() method 
+
+# importing calendar module 
+import calendar 
+
+# calling setfirstweekday() function 
+val = calendar.setfirstweekday(calendar.SUNDAY) 
+
+# returns None 
+print(val) 
+
