8ad3ac6a6cf23dfc3ebe6a82f4e756d2449143c1
archanacoder <44294498+archanacoder@users.noreply.github.com>
2019-10-01 07:20:15 +0530

    made changes!

--- cpp/factorial.cpp
+++ cpp/factorial.cpp
@@ -1,19 +1,25 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-
-map<int,int> f;
-
-int fact(int n){
-
-	if(n==1 || n==0)
-		return 1;
-	else if(f[n])
-		return f[n];
-	else
-		return f[n]=n*fact(n-1);
+#include<stdio.h>
+int find_factorial(int);
+int main()
+{
+   int num, fact;
+   //Ask user for the input and store it in num
+   printf("\nEnter any integer number:");
+   scanf("%d",&num);
+ 
+   //Calling our user defined function
+   fact =find_factorial(num);
+ 
+   //Displaying factorial of input number
+   printf("\nfactorial of %d is: %d",num, fact);
+   return 0;
 }
-
-int main(){
-	cout<<fact(5);
+int find_factorial(int n)
+{
+   //Factorial of 0 is 1 
+   if(n==0)
+      return(1);
+ 
+   //Function calling itself: recursion
+   return(n*find_factorial(n-1));
 }
