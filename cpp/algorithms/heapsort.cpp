e16869c0fec4c0fbdc11d8cb3c540b31af0e2585
Rohit Kumar <rkrohitkr01@gmail.com>
2019-10-02 15:33:39 +0530

    updated heapsort.cpp
    removes extra spaces and added a return statement

--- cpp/algorithms/heapsort.cpp
+++ cpp/algorithms/heapsort.cpp
@@ -1,20 +1,19 @@
-#include<iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-
 
 void heapify(int a[],int i,int heapSize)
 {
   int large=i;
   int l= (2*i)+1;
-  int r=(2*i)+2
-  ;
+  int r=(2*i)+2;
 
-  if(l<heapSize && a[l]>a[i]) large=l;
-  else large=i;
+  if(l<heapSize && a[l]>a[i]) 
+    large=l;
+  else 
+    large=i;
 
-  if(r<heapSize && a[r]>a[large]) large=r;
+  if(r<heapSize && a[r]>a[large]) 
+    large=r;
   if(large!=i)
   {
     int temp=a[i];
@@ -23,16 +22,11 @@ void heapify(int a[],int i,int heapSize)
 
     heapify(a,large,heapSize);
   }
-
-
-
 }
-
-
-
 void heapSort(int a[],int n)
 {
-  for(int i=(n/2)-1;i>=0;i--) heapify(a,i,n);
+  for(int i=(n/2)-1;i>=0;i--) 
+    heapify(a,i,n);
   for(int i=n-1;i>=0;i--)
   {
     int temp=a[0];
@@ -44,26 +38,22 @@ void heapSort(int a[],int n)
 
 }
 
-
 int main()
 {
   int n;
- cin>>n;
+  cin>>n;
   int a[n];
-for(int i=0;i<n;i++)
-{
-  cin>>a[i];
-}
+  
+  for(int i=0;i<n;i++)
+  {
+    cin>>a[i];
+  }
 
-heapSort(a,n);
+  heapSort(a,n);
 
-for(int i=0;i<n;i++)
-{
-  cout<<a[i]<<" ";
-}
-
-
-
-
-
+  for(int i=0;i<n;i++)
+  {
+    cout<<a[i]<<" ";
+  }
+  return 0;
 }
