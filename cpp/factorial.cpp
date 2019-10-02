68b853f21c2525740cafe83094bc25dbd3370fd5
CODESTIEN <32845547+Tarunyo@users.noreply.github.com>
2019-10-02 09:17:10 +0530

    it wasn

--- cpp/factorial.cpp
+++ cpp/factorial.cpp
@@ -2,18 +2,16 @@
 
 using namespace std;
 
-map<int,int> f;
-
-int fact(int n){
-
-	if(n==1 || n==0)
-		return 1;
-	else if(f[n])
-		return f[n];
-	else
-		return f[n]=n*fact(n-1);
-}
-
-int main(){
-	cout<<fact(5);
+int main()
+{
+    unsigned int n;
+    unsigned long long factorial = 1;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    for(int i = 1; i <=n; ++i)
+    {
+        factorial *= i;
+    }
+    cout << "Factorial of " << n << " = " << factorial;    
+    return 0;
 }
