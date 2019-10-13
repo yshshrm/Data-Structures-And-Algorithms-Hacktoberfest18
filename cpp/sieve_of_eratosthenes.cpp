3efe48bffd625b73cd0c0331d7f33bf7da413474
Subhadeep Deyashi <subhadeepdeyashi1@gmail.com>
2019-10-13 16:53:03 +0530

    Update sieve_of_eratosthenes.cpp
    Reduced the time complexity a little bit.

--- cpp/sieve_of_eratosthenes.cpp
+++ cpp/sieve_of_eratosthenes.cpp
@@ -1,26 +1,27 @@
 ﻿//Prime numbers using Sieve of Eratosthenes
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     ios::sync_with_stdio(false);
     int n;
-    cin>>n;
+    cin >> n;
     vector <int> prime(n+1);
-    prime[0] = 1;
-    prime[1] = 1;
-    for(int i=2;i<=ceil(sqrt(n));i++)
+    prime[0] = prime[1] = 1;
+    
+    for(int i = 2; i*i <= n; ++i)
     {
-        if(prime[i]==0)
+        if(prime[i] == 0)
         {
-            for(int j=2;i*j <= n;j++)
-               prime[i*j] = 1;
+            for(int j = i*i; j <= n; j += i)
+               prime[j] = 1;
         }
     }
-    for(int j=0;j<=n;j++)
+    for(int j = 2;j <= n; ++j)
     {
-        if(prime[j]==0)
-        cout<<j<<" ";
+        if(prime[j] == 0)
+        cout << j << " ";
     }
     return 0;
 }
