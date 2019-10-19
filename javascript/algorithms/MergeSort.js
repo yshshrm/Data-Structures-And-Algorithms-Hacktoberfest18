0b2ae04f54a0a76773a426417751c0995cbf8fed
AashifAhamed <aashifahamed0796@gmail.com>
2019-10-19 22:59:42 +0530

    Create MergeSort.js
    merge Sort added and tested

---
+++ javascript/algorithms/MergeSort.js
@@ -0,0 +1,36 @@
+var a = [149, 520, 50, 76, 270, 684, 128, 584, 290];
+ 
+function merge(left, right)
+{
+    var result = [];
+ 
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+ 
+    while (left.length)
+        result.push(left.shift());
+ 
+    while (right.length)
+        result.push(right.shift());
+ 
+    return result;
+}
+ 
+function mergeSort(arr)
+{
+    if (arr.length < 2)
+        return arr;
+ 
+    var middle = parseInt(arr.length / 2);
+    var left   = arr.slice(0, middle);
+    var right  = arr.slice(middle, arr.length);
+ 
+    return merge(mergeSort(left), mergeSort(right));
+}
+ 
+console.log(mergeSort(a));
