ed2c9774be1baf34ae4afe53df9271c866078b36
b18030 <tushar22.tg.tg@gmail.com>
2019-10-20 11:36:03 +0530

    Code Added in Stacks

---
+++ 2.stacks/1. StackusingArray/StackUse.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include "StackUsingArray.cpp"
+
+int main() {
+	StackUsingArray s(4);
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	s.push(40);
+	s.push(50);
+
+
+	cout << s.top() << endl;
+
+	cout << s.pop() << endl;
+	cout << s.pop() << endl;
+	cout << s.pop() << endl;
+
+	cout << s.size() << endl;
+
+	cout << s.isEmpty() << endl;
+
+}
+
