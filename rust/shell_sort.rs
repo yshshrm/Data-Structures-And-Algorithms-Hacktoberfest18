66aeabbc153370fe9f0c4d941fe41415772660df
barrotsteindev <barrotstein@gmail.com>
2019-10-20 19:03:20 +0300

    rust shell sort

---
+++ rust/shell_sort.rs
@@ -0,0 +1,21 @@
+fn shell_sort<T: Ord>(arr: &mut [T]) {
+    let mut gap = (arr.len() / 2).clone();
+    while gap > 0 {
+        for i in gap..arr.len() {
+            let mut j = i;
+            while j >= gap && arr[j - gap] > arr[i] {
+                arr.swap(j, j - gap);
+                j -= gap;
+            }
+        }
+        gap = gap / 2;
+    }
+}
+
+fn main() {
+    let mut array = vec![0, 8, 4, 7, 1, 9, 10, 5, 2, 11, 3];
+    println!("{:?}", array);
+    shell_sort(&mut array);
+    println!("{:?}", array);
+
+}
