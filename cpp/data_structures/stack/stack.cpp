d8930dc788a66f58f84c59a8c991030ec65d0f2e
ronythakur <56474223+ronythakur@users.noreply.github.com>
2019-10-23 13:02:23 +0530

    stack.cpp

---
+++ cpp/data_structures/stack/stack.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+#include <stack> 
+using namespace std; 
+
+void showstack(stack <int> s) 
+{ 
+	while (!s.empty()) 
+	{ 
+		cout << '\t' << s.top(); 
+		s.pop(); 
+	} 
+	cout << '\n'; 
+} 
+
+int main () 
+{ 
+	stack <int> s; 
+	s.push(10); 
+	s.push(30); 
+	s.push(20); 
+	s.push(5); 
+	s.push(1); 
+
+	cout << "The stack is : "; 
+	showstack(s); 
+
+	cout << "\ns.size() : " << s.size(); 
+	cout << "\ns.top() : " << s.top(); 
+
+
+	cout << "\ns.pop() : "; 
+	s.pop(); 
+	showstack(s); 
+
+	return 0; 
+} 
