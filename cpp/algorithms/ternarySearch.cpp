afc513fb10a1a197fe38aa9009fc4784e16f772f
Gilbert Raju <32704517+gilbertraju@users.noreply.github.com>
2019-10-29 01:49:41 +0530

    Create Linear_Search_algo.py

--- cpp/algorithms/ternarySearch.cpp
+++ cpp/algorithms/ternarySearch.cpp
@@ -1,40 +1,51 @@
-/*
-	terniary search finds position of an element in a UNIMODAL function or array filled
-	* unimodally
-	*  https://en.wikipedia.org/wiki/Unimodality
- */
- #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-const int SIZE = 10000005;
-int arr[SIZE];
-int ternary_search(int l,int r, int x)
+
+int ternary_search(vector<int> ar,int l,int r, int x)
 {
     if(r>=l)
     {
         int mid1 = l + (r-l)/3;
-        int mid2 = r -  (r-l)/3;
-        if(arr[mid1] == x)
+        int mid2 = r - (r-l)/3;
+        if(ar[mid1] == x)
             return mid1;
-        if(arr[mid2] == x)
+        if(ar[mid2] == x)
             return mid2;
-        if(x<arr[mid1])
-            return ternary_search(l,mid1-1,x);
-        else if(x>arr[mid2])
-            return ternary_search(mid2+1,r,x);
+        if(x<ar[mid1])
+            return ternary_search(ar,l,mid1-1,x);
+        else if(x>ar[mid2])
+            return ternary_search(ar,mid2+1,r,x);
         else
-            return ternary_search(mid1+1,mid2-1,x);
+            return ternary_search(ar,mid1+1,mid2-1,x);
 
     }
     return -1;
 }
+
+
 int main()
 {
-	int sz = 100;
-	for(int i=0;i<sz;i++)
-		arr[i] = i*2;
-	int pos = ternary_search(0,sz,10);
-	if (pos == -1)
-		cout <<"NOT FOUND\n";
-	else cout <<"FOUND AT "<<pos+1<<endl;
-return 0;
+   vector <int>v;
+   int n;
+   cout<<"Enter size of list\n";
+   cin>>n;
+   int no,k;
+   cout<<"Enter nos\n";
+   while(n--)
+   {
+   cin>>no;
+   v.push_back(no);
+   } 
+    cout<<"Enter number you want to search"<<endl;
+   
+    cin>>k;
+    int l=0;
+    int r=v.size()-1;
+    int found = ternary_search(v,l,r,k);
+    if(found == -1)
+        std::cout<<"Couldn't find number"<<endl;
+     else
+     {cout<<"Number is found at position "<<++found;
+     }   
+    return 0;
 }
