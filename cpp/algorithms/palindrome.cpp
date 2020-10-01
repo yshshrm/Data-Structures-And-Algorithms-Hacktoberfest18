96c78b04ff856c3f5f6051c536aa9ad604975875
Aarhan212 <72205689+Aarhan212@users.noreply.github.com>
2020-10-01 21:54:12 +0530

    basic changes

--- cpp/algorithms/palindrome.cpp
+++ cpp/algorithms/palindrome.cpp
@@ -1,19 +1,22 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-
+#include<iostream>
+#include<cstring>
 using namespace std;
 int main()
 {
-	cout<<"enter a string : ";
-	string arr,h;
-	getline(cin,arr);
-	h=arr;
-	reverse(arr.begin(),arr.end());
-	if(h==arr)
-		cout<<"pali \n";
-	else
-		cout<<"not pali \n";
-	
-	 return 0;
+string str;
+cin>>str;
+int l =str.length();
+int f=0,j=l-1;
+  for(int i=0;i<=l/2;++i)
+  {
+    if(str[i]!=str[j--])
+    {
+      cout<<endl<<"Not a palindrome";
+      break;
+    }
+    else
+      f++;
+  }
+  if(f!=0)
+    cout<<endl<<"Palindrome";
 }
--- php/algorithms/quickSort.php
+++ php/algorithms/quickSort.php
@@ -1,30 +1,28 @@
 <?php
-
-function quickSort($array)
-{
-    $length = count($array);
-
-    if($length <= 1){
-        return $array;
-    }
-    else{
-        $pivot = $array[0];
-        $left = $right = array();
-
-        for($i = 1; $i < count($array); $i++)
-        {
-            if($array[$i] < $pivot){
-                $left[] = $array[$i];
-            }
-            else{
-                $right[] = $array[$i];
-            }
-        }
-        return array_merge(quickSort($left), array($pivot), quickSort($right));
-    }
+function quick_sort($my_array)
+ {
+	$loe = $gt = array();
+	if(count($my_array) < 2)
+	{
+		return $my_array;
+	}
+	$pivot_key = key($my_array);
+	$pivot = array_shift($my_array);
+	foreach($my_array as $val)
+	{
+		if($val <= $pivot)
+		{
+			$loe[] = $val;
+		}elseif ($val > $pivot)
+		{
+			$gt[] = $val;
+		}
+	}
+	return array_merge(quick_sort($loe),array($pivot_key=>$pivot),quick_sort($gt));
 }
-$arrayTest = array(2, 1, 0, 10, -6, 12, 22);
-echo "Unsorted Array\n";
-echo implode(', ',$arrayTest );
-echo "\nSorted Array\n";
-echo implode(', ',quickSort($arrayTest)). PHP_EOL;
+ 
+$my_array = array(3, 0, 2, 5, -1, 4, 1);
+echo 'Original Array : '.implode(',',$my_array).'\n';
+$my_array = quick_sort($my_array);
+echo 'Sorted Array : '.implode(',',$my_array);
+?>
