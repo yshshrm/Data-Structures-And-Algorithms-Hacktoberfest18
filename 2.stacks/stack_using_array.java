cba7becf1e16074691a3dd102874008bdb0e2534
Akrith H Nayak <akrithnayak2000@gmail.com>
2020-10-17 12:32:52 +0530

    Update stack_using_array
    Added comments to the code to improve code readability.

--- 2.stacks/stack_using_array.java
+++ 2.stacks/stack_using_array.java
@@ -55,9 +55,9 @@ class Main {
     public static void main(String args[]) 
     { 
         Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        s.push(10); // push 10 to stack
+        s.push(20); // push 20 to stack
+        s.push(30); // push 30 to stack
+        System.out.println(s.pop() + " Popped from stack"); // pop top (30) from stack and print it.
     } 
 } 
