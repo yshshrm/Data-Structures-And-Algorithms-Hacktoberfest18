28593378f1728f56b48ef5514a82d7f7a7c082d2
azizur13 <73368231+azizur13@users.noreply.github.com>
2020-10-23 19:28:07 -0400

    Create ConvertBinaryToInt.java

---
+++ java/ConvertBinaryToInt.java
@@ -0,0 +1,9 @@
+/** Converting binary numbers in an array list to int **/
+public int getDecimalValue(ListNode head) {
+        int num = head.val;
+        while (head.next != null) {
+            num = (num << 1) | head.next.val;
+            head = head.next;    
+        }
+        return num;
+    }
