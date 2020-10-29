286ae70a712e57c326e8d1eff8eda694d391b965
SWATI SINGH <amenta10203066@gmail.com>
2020-10-29 20:43:03 +0530

    Create Reverse a doubly linked list.cpp
    Hey added a LinkedList algorithm

---
+++ 1.linked list/Reverse a doubly linked list.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DoublyLinkedListNode {
+    public:
+        int data;
+        DoublyLinkedListNode *next;
+        DoublyLinkedListNode *prev;
+
+        DoublyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    public:
+        DoublyLinkedListNode *head;
+        DoublyLinkedListNode *tail;
+
+        DoublyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+                node->prev = this->tail;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_doubly_linked_list(DoublyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_doubly_linked_list(DoublyLinkedListNode* node) {
+    while (node) {
+        DoublyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the reverse function below.
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+
+
+Node* Reverse(Node* head)
+{
+  // If empty list, return
+  if (!node) return NULL;
+  
+  // Otherwise, swap the next and prev
+  Node *temp = node->next;
+  node->next = node->prev;
+  node->prev = temp;
+  
+  // If the prev is now NULL, the list
+  // has been fully reversed
+  if (!node->prev) return node;
+  
+  // Otherwise, keep going
+  return Reverse(node->prev);
+}
+
+
+int main()
