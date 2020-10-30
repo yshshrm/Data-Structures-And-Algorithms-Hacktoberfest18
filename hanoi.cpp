647d974ed5fb039d15ec2b47351d648e2e2c75d2
Aditya Gupta <53346087+xcyberpunkx0@users.noreply.github.com>
2020-10-31 00:29:19 +0530

    Create hanoi.cpp

---
+++ hanoi.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void TOH(int n,char x,char y,char z){
+    if(n==1)
+        std::cout<<"Move top disk from "<<x<<" to "<<z<<std::endl;
+    else{
+        TOH(n-1,x,z,y);
+        TOH(1,x,y,z);
+        TOH(n-1,y,x,z);
+    }
+    
+}
+
+int main() {
+    using namespace std;
+    int num;
+    char x,y,z;
+    cout << "Hello, How many disks you want to make your tower with?\n";
+    cin>>num;
+    cout<<"Name your towers respetively\n";
+    cin>>x>>y>>z;
+    TOH(num,x,y,z);
+    return 0;
+}
