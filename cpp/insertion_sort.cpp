28f88fb2d8b6fadb37d73aee9676e2078625841d
Karan <karanmeena237@gmail.com>
2021-05-18 16:13:43 +0530

    provided simple solution without using pointers and simple sorting and taking input of array element'

--- cpp/insertion_sort.cpp
+++ cpp/insertion_sort.cpp
@@ -8,10 +8,16 @@ void swap(int* a, int* b)	//Swaps two elements of array
 	*b=t;
 }
 
-int main()
-{
-	int arr[] = {5,2,4,8,9,1,3,10,6,7};
-	int n = sizeof(arr)/sizeof(arr[0]);
+int main(){
+// {
+// 	int  arr[] = {5,2,4,8,9,1,3,10,6,7};
+// 	int n = sizeof(arr)/sizeof(arr[0]);
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+	cin>>arr[i];
+}
 
 	int j,k;
 	for(int i=0;i<n;i++)
@@ -27,7 +33,10 @@ int main()
 		*/
 		while(arr[j]<arr[k] && k>=0)
 		{
-			swap(&arr[j], &arr[k]);
+			// swap(&arr[j], &arr[k]);
+			int temp=arr[j];
+	arr[j]=arr[k];        //We are meaking temp as a temporary container to store the value for some time like if we have arr[j]=7 and arr[k]=6 to swap them if we store arr[j] = arr[k]; then the arr[j] will get the value 6 but after that the value which arr[j] had i.e 7 will be completely gone 
+	arr[k]=temp;
 			j--;
 			k--;
 		}
