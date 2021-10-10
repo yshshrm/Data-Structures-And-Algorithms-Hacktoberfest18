8a8e648114923fb2ef929150c9458b5f5f580c9e
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2021-10-10 10:29:44 +0530

    Sorting Algorithms
    Added these Sorting Algos in DSA folder

---
+++ Data-Structures-And-Algorithms-Hacktoberfest18-master/Wave_Sort.cpp
@@ -0,0 +1,50 @@
+// Name: JHALA DEVRAJSINH SHRIPALSINH
+// Date: 01/07/2021
+// Purpose: Wave Sort
+// Wave form like arr[0]>= arr[1] <= arr[2] >= arr[3] <= arr[4] >= ...
+
+// Time Complexity:
+// O(N/2) === O(N)
+// Final time complexity === O(N)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+void wavesort(int arr[], int n)
+{
+    for(int i=1; i<n; i+=2)
+    {
+        if(arr[i] > arr[i-1])
+        {
+            swap(arr,i,i-1);
+        }
+
+        if(arr[i] > arr[i+1] && i <= n-2)
+        {
+            swap(arr, i,i+1);
+        }
+    }
+}
+
+
+
+int main()
+{
+
+    int arr[] = {1,3,4,7,5,6,2};
+    wavesort(arr,7);
+
+    for(int i=0;i<7;i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
