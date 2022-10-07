ae1dfe90b2b993ea6ca08713e35567c65254eb46
sajalgupta <sajalgupta1982002.sg@gmail.com>
2022-10-07 11:28:58 +0530

    Insertion sort is completed in c

--- c/insertion_sort.c
+++ c/insertion_sort.c
@@ -1,6 +1,41 @@
+#include<stdio.h>
+ void insertion_sort(int arr[], int n)
+ {
+     int i, key, j;
+    for (i = 1; i < n; i++)
+     {
+        key = arr[i];
+        j = i - 1;
+  
+       
+        while (j >= 0 && arr[j] > key)
+         {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+ }
+int main()
+{
+    int n , j;
+    printf("Enter the count of element:");
+    scanf("%d",&n);
+     int arr[n];
+     printf("Enter the elements in the array:");
+     for(int i=0;i<n;i++)
+     {
+        scanf("%d",&arr[i]);
+     }
 
-#include <stdio.h>
-int main(){
-    printf("Hello world");
-    return 0;
+    insertion_sort(arr, n);
+
+    printf("Sorted element:\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d \t", arr[i]);
+    }
+     
+     return 0;
+
 }
